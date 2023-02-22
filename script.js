3fd2cb0bd0c16c586127cc3f825fac526720d7d6
Jared <jared02martinez@gmail.com>
2023-02-22 11:50:40 -0800

    loading state and responsiveness

--- script.js
+++ script.js
@@ -2,31 +2,31 @@ const searchElement = document.querySelector(".section__title")
 const movieListElement = document.querySelector(".movie__list")
 
 async function renderMovies(filter) {
+    window.scrollBy(0, 400)
     const search = document.querySelector(".header__input").value
     const movies = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=eaf2afff&s=${search}`)
     const moviesData = await movies.json()
-    movieListElement.classList += " movies__loading"
 
-    setTimeout(() => {
         if (filter === "MOVIES") {
             let filteredMovies = moviesData.Search.filter((movie) => movie.Type === "movie")
-            movieListElement.classList.remove("movies__loading")
             movieListElement.innerHTML = filteredMovies.map((movie => movieHTML(movie))).join("")
         }
         else if (filter === "SERIES") {
             let filteredMovies = moviesData.Search.filter((movie) => movie.Type === "series")
-            movieListElement.classList.remove("movies__loading")
             movieListElement.innerHTML = filteredMovies.map((movie => movieHTML(movie))).join("")
         }
         else if (filter === "EPISODES") {
             let filteredMovies = moviesData.Search.filter((movie) => movie.Type === "episode")
-            movieListElement.classList.remove("movies__loading")
             movieListElement.innerHTML = filteredMovies.map((movie => movieHTML(movie))).join("")
         }
         else {
-            movieListElement.innerHTML = moviesData.Search.map((movie => movieHTML(movie))).join("")
+            movieListElement.classList += " movies__loading"
+            movieListElement.innerHTML = `<i class="fas fa-spinner movies__loading--spinner"></i>`
+            setTimeout(() => {
+                movieListElement.classList.remove("movies__loading")
+                movieListElement.innerHTML = moviesData.Search.map((movie => movieHTML(movie))).join("")
+            }, 1000);
         }
-    }, 10000);
 
     searchElement.innerHTML = `Search Results for "${search}":`
 }
@@ -38,7 +38,7 @@ function movieHTML(movie) {
         <div class="movie__description">
             <div class="movie__title">${movie.Title}</div>
             <p class="movie__year">Year: ${movie.Year}</p>
-            <p class="movie__type">Type: ${movie.Type}</p>
+            <p class="movie__type">Type: ${movie.Type.charAt(0).toUpperCase() + movie.Type.slice(1)}</p>
         </div>
     </div>`
 }
